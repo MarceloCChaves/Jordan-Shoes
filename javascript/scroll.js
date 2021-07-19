29b4ae9c95b1c9f02b5a70950a551632fd80ba07
MarceloCChaves <62251064+MarceloCChaves@users.noreply.github.com>
2021-07-18 21:54:32 -0300

    Initial commit

---
+++ javascript/scroll.js
@@ -0,0 +1,8 @@
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "50px",
+    duration: 2000,
+    reset: true,
+  });
+
+  ScrollReveal().reveal(".products-animation", { delay: 200 });
---
+++ javascript/type.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#typed',{
+    stringsElement: '#typed-strings',
+    typeSpeed: 30,
+    loop: true,
+    backDelay: 500,
+    startDelay: 1000,
+});
